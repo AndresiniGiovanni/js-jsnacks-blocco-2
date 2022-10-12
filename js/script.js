080373534dddcbd03797a7121c5fe9757b92f5fc
AndresiniGiovanni <gio.andresini@hotmail.it>
2022-10-12 15:41:16 +0200

    snack 1

--- js/script.js
+++ js/script.js
@@ -2,5 +2,25 @@
 // Inserisci un numero, se è pari stampa il numero,
 //  se è dispari stampa il numero successivo.
 
-let button = document.getElementById("tasto");
+let button = document.getElementById("print");
 
+function numeri() {
+  const number = document.getElementById("numbers").value;
+  let div = document.querySelector("#mydiv");
+
+  if (number % 2 == 0) {
+    console.log("Il numero é pari.");
+    div.className = "div";
+    div.innerHTML = "pari";
+
+  } else {
+    console.log("Il numero é dispari.");
+    div.className = "div";
+    div.innerHTML = "dispari";
+
+    div.append(div);
+  }
+
+  button.addEventListener("click", numeri);
+}
+numeri();
